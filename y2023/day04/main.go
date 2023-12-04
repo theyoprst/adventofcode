a59cbee3c96703cf1e13304d96f9e38b2c0edcc7
Slava Ezhkin <yozhkin@gmail.com>
2023-12-04 06:48:37 +0100

    Simpify day 4 solution.

--- y2023/day04/main.go
+++ y2023/day04/main.go
@@ -1,4 +1,4 @@
-// 6:12 - 6:40
+// 6:00 - 6:12 - 6:40
 package main
 
 import (
@@ -9,15 +9,6 @@ import (
 	"github.com/theyoprst/adventofcode/helpers"
 	"github.com/theyoprst/adventofcode/must"
 )
-
-func mustParseList(s string) []int {
-	split := strings.Fields(s)
-	var res []int
-	for _, x := range split {
-		res = append(res, must.Atoi(x))
-	}
-	return res
-}
 
 func intersect(a, b []int) []int {
 	var res []int
@@ -36,17 +27,15 @@ func main() {
 	for i, line := range lines {
 		_, line = must.Split2(line, ":")
 		winsStr, haveStr := must.Split2(line, "|")
-		wins := mustParseList(strings.TrimSpace(winsStr))
-		have := mustParseList(strings.TrimSpace(haveStr))
-		in := intersect(wins, have)
-		if len(in) > 0 {
-			ans1 += 1 << (len(in) - 1)
-		}
-		inc := copies[i] + 1
-		ans2 += inc
-		if len(in) > 0 {
-			for j := 0; j < len(in); j++ {
-				copies[i+j+1] += copies[i] + 1
+		wins := must.ParseInts(strings.TrimSpace(winsStr))
+		have := must.ParseInts(strings.TrimSpace(haveStr))
+		k := len(intersect(wins, have))
+		copies[i]++
+		ans2 += copies[i]
+		if k > 0 {
+			ans1 += 1 << (k - 1)
+			for j := 0; j < k; j++ {
+				copies[i+j+1] += copies[i]
 			}
 		}
 	}
