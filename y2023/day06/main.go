f9bffa376327d1a9b38bd861715fa2cfa28d5052
Slava Ezhkin <yozhkin@gmail.com>
2023-12-06 07:31:18 +0100

    2023 Day 06 solution

---
+++ y2023/day06/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/theyoprst/adventofcode/aoc"
+)
+
+func main() {
+	ans := 1
+	lines := aoc.ReadInputLines()
+	tt := aoc.Ints(lines[0])
+	rr := aoc.Ints(lines[1])
+	for i := range tt {
+		wins := 0
+		for a := 0; a <= tt[i]; a++ {
+			b := tt[i] - a
+			if a*b > rr[i] {
+				wins++
+			}
+		}
+		ans *= wins
+	}
+
+	fmt.Println("Answer:", ans)
+}
