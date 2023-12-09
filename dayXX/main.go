d760f4b8370ad1b74dd96e6e8eaa2267ba089b7b
Slava Ezhkin <yozhkin@gmail.com>
2023-12-09 19:12:45 +0100

    improve template

--- dayXX/main.go
+++ dayXX/main.go
@@ -2,17 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"slices"
 
 	"github.com/theyoprst/adventofcode/aoc"
 )
 
-func main() {
-	ans1, ans2 := 0, 0
-	lines := aoc.ReadInputLines()
-	for i, line := range lines {
-		_, _ = i, line
-	}
+func SolvePart1(lines []string) any {
+	return 0
+}
 
-	fmt.Println("Part 1:", ans1)
-	fmt.Println("Part 2:", ans2)
+func SolvePart2(lines []string) any {
+	return 0
+}
+
+func main() {
+	lines1 := aoc.ReadInputLines()
+	lines2 := slices.Clone(lines1)
+	var cmd string
+	if len(os.Args) > 1 {
+		cmd = os.Args[1]
+	}
+	if cmd != "part2" || cmd == "part1" {
+		fmt.Println("Part 1:", SolvePart1(lines1))
+	}
+	if cmd != "part1" || cmd == "part2" {
+		fmt.Println("Part 2:", SolvePart2(lines2))
+	}
 }
