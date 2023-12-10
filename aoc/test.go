dee9d28593d7d3a9ac96f57fdfbba18ff88c53b7
Slava Ezhkin <yozhkin@gmail.com>
2023-12-10 13:10:03 +0100

    Move common test logic to aoc package.

---
+++ aoc/test.go
@@ -0,0 +1,89 @@
+package aoc
+
+import (
+	"fmt"
+	"os"
+	"reflect"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+type Input struct {
+	Path      string
+	WantPart1 string
+	WantPart2 string
+}
+
+type Solver func([]string) any
+
+func getFunctionName(temp interface{}) string {
+	path := runtime.FuncForPC(reflect.ValueOf(temp).Pointer()).Name()
+	return path[strings.LastIndex(path, ".")+1:]
+}
+
+func RunTests(t *testing.T, inputs []Input, solversPart1 []Solver, solversPart2 []Solver) {
+	type testCase struct {
+		path string
+		want string
+	}
+	var testCasesPart1 []testCase
+	var testCasesPart2 []testCase
+	for _, input := range inputs {
+		if input.WantPart1 != "" {
+			testCasesPart1 = append(testCasesPart1, testCase{
+				path: input.Path,
+				want: input.WantPart1,
+			})
+		}
+		if input.WantPart2 != "" {
+			testCasesPart2 = append(testCasesPart2, testCase{
+				path: input.Path,
+				want: input.WantPart2,
+			})
+		}
+	}
+
+	type testSuite struct {
+		name      string
+		solver    func([]string) any
+		testCases []testCase
+	}
+
+	var testSuites []testSuite
+	for _, solvePart1 := range solversPart1 {
+		testSuites = append(testSuites, testSuite{
+			name:      getFunctionName(solvePart1),
+			solver:    solvePart1,
+			testCases: testCasesPart1,
+		})
+	}
+	for _, solvePart2 := range solversPart2 {
+		testSuites = append(testSuites, testSuite{
+			name:      getFunctionName(solvePart2),
+			solver:    solvePart2,
+			testCases: testCasesPart2,
+		})
+	}
+
+	for _, suite := range testSuites {
+		t.Run(suite.name, func(t *testing.T) {
+			for _, testCase := range suite.testCases {
+				t.Run(testCase.path, func(t *testing.T) {
+					f, err := os.Open(testCase.path)
+					if err != nil {
+						t.Fatal(err)
+					}
+					defer func() {
+						_ = f.Close()
+					}()
+					ans := suite.solver(ReadLines(f))
+					ansStr := fmt.Sprint(ans)
+					if ansStr != testCase.want {
+						t.Errorf("Got answer %q, want %q", ansStr, testCase.want)
+					}
+				})
+			}
+		})
+	}
+}
--- y2023/day10/main_test.go
+++ y2023/day10/main_test.go
@@ -1,87 +1,25 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"testing"
 
 	"github.com/theyoprst/adventofcode/aoc"
 )
 
-type Input struct {
-	Path      string
-	WantPart1 string
-	WantPart2 string
-}
-
-var inputs []Input = []Input{
-	{Path: "input_ex0.txt", WantPart1: "4", WantPart2: "1"},
-	{Path: "input_ex1.txt", WantPart1: "4", WantPart2: "1"},
-	{Path: "input_ex2.txt", WantPart1: "8", WantPart2: "1"},
-	{Path: "input_ex3.txt", WantPart2: "4"},
-	{Path: "input_ex4.txt", WantPart2: "8"},
-	{Path: "input.txt", WantPart1: "6897", WantPart2: "367"},
-}
-
 func Test(t *testing.T) {
-	type testCase struct {
-		path string
-		want string
+	inputs := []aoc.Input{
+		{Path: "input_ex0.txt", WantPart1: "4", WantPart2: "1"},
+		{Path: "input_ex1.txt", WantPart1: "4", WantPart2: "1"},
+		{Path: "input_ex2.txt", WantPart1: "8", WantPart2: "1"},
+		{Path: "input_ex3.txt", WantPart2: "4"},
+		{Path: "input_ex4.txt", WantPart2: "8"},
+		{Path: "input.txt", WantPart1: "6897", WantPart2: "367"},
 	}
-	var testCasesPart1 []testCase
-	var testCasesPart2 []testCase
-	for _, input := range inputs {
-		if input.WantPart1 != "" {
-			testCasesPart1 = append(testCasesPart1, testCase{
-				path: input.Path,
-				want: input.WantPart1,
-			})
-		}
-		if input.WantPart2 != "" {
-			testCasesPart2 = append(testCasesPart2, testCase{
-				path: input.Path,
-				want: input.WantPart2,
-			})
-		}
+	solversPart1 := []aoc.Solver{
+		SolvePart1,
 	}
-
-	type testSuite struct {
-		name      string
-		solver    func([]string) any
-		testCases []testCase
+	solversPart2 := []aoc.Solver{
+		SolvePart2,
 	}
-
-	testSuites := []testSuite{
-		{
-			name:      "Part1",
-			solver:    SolvePart1,
-			testCases: testCasesPart1,
-		},
-		{
-			name:      "Part2",
-			solver:    SolvePart2,
-			testCases: testCasesPart2,
-		},
-	}
-
-	for _, suite := range testSuites {
-		t.Run(suite.name, func(t *testing.T) {
-			for _, testCase := range suite.testCases {
-				t.Run(testCase.path, func(t *testing.T) {
-					f, err := os.Open(testCase.path)
-					if err != nil {
-						t.Fatal(err)
-					}
-					defer func() {
-						_ = f.Close()
-					}()
-					ans := suite.solver(aoc.ReadLines(f))
-					ansStr := fmt.Sprint(ans)
-					if ansStr != testCase.want {
-						t.Errorf("Got answer %q, want %q", ansStr, testCase.want)
-					}
-				})
-			}
-		})
-	}
+	aoc.RunTests(t, inputs, solversPart1, solversPart2)
 }
