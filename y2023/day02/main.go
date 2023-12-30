e73451b7c808a7c34ff63728c3ba9d57b46bdc66
Slava Ezhkin <yozhkin@gmail.com>
2023-12-30 09:04:43 +0100

    2023 day 02 simplify

--- y2023/day02/main.go
+++ y2023/day02/main.go
@@ -2,32 +2,17 @@ package main
 
 import (
 	"log"
-	"strings"
+	"regexp"
 
 	"github.com/theyoprst/adventofcode/aoc"
 	"github.com/theyoprst/adventofcode/must"
 )
 
-type Handful struct {
-	r, g, b int
-}
-
-func union(hh []Handful) Handful {
-	var res Handful
-	for _, h := range hh {
-		res.r = max(res.r, h.r)
-		res.g = max(res.g, h.g)
-		res.b = max(res.b, h.b)
-	}
-	return res
-}
-
 func SolvePart1(lines []string) any {
 	var ans int
 	for gameI, line := range lines {
-		handfuls := parseGame(line)
-		u := union(handfuls)
-		if u.r <= 12 && u.g <= 13 && u.b <= 14 {
+		c := parseColors(line)
+		if c["red"] <= 12 && c["green"] <= 13 && c["blue"] <= 14 {
 			ans += gameI + 1
 		}
 	}
@@ -37,39 +22,21 @@ func SolvePart1(lines []string) any {
 func SolvePart2(lines []string) any {
 	var ans int
 	for _, line := range lines {
-		handfuls := parseGame(line)
-		u := union(handfuls)
-		ans += u.r * u.b * u.g
+		c := parseColors(line)
+		ans += c["red"] * c["green"] * c["blue"]
 	}
 	return ans
 }
 
-func parseGame(game string) []Handful {
-	_, game = must.Split2(game, ":")
-	var handfuls []Handful
-	for _, try := range strings.Split(game, ";") {
-		var handful Handful
-		for _, colorN := range strings.Split(try, ",") {
-			colorN = strings.TrimSpace(colorN)
-			nStr, color := must.Split2(colorN, " ")
-			n := must.Atoi(nStr)
-			switch color {
-			case "red":
-				must.Equal(handful.r, 0)
-				handful.r = n
-			case "green":
-				must.Equal(handful.g, 0)
-				handful.g = n
-			case "blue":
-				must.Equal(handful.b, 0)
-				handful.b = n
-			default:
-				panic("unreachable")
-			}
-		}
-		handfuls = append(handfuls, handful)
+var reColor = regexp.MustCompile(` (\d+) +(\w+)`)
+
+func parseColors(game string) map[string]int {
+	maxColors := map[string]int{}
+	for _, match := range reColor.FindAllStringSubmatch(game, -1) {
+		color := match[2]
+		maxColors[color] = max(maxColors[color], must.Atoi(match[1]))
 	}
-	return handfuls
+	return maxColors
 }
 
 var (
