114328a297cdb5ae6ee089a7c52c09105057597e
Slava Ezhkin <yozhkin@gmail.com>
2023-12-31 16:56:25 +0100

    2023 day 03 simplify

--- y2023/day03/main.go
+++ y2023/day03/main.go
@@ -6,17 +6,12 @@ import (
 	"github.com/theyoprst/adventofcode/aoc/fld"
 )
 
-type Point struct {
-	i, j int
-}
-
-func Solve(lines []string) (ans1, ans2 int) {
+func SolvePart1(lines []string) any {
 	field := fld.NewByteField(lines).AddBorder('.')
-	allGears := map[Point][]int{}
+	var ans int
 	for i, row := range field {
 		isN := false
 		isAdj := false
-		gears := containers.NewSet[Point]()
 		var n int
 		for j, ch := range row {
 			if aoc.IsDigit(ch) {
@@ -28,8 +23,39 @@ func Solve(lines []string) (ans1, ans2 int) {
 						if ch != '.' && !aoc.IsDigit(ch) {
 							isAdj = true
 						}
+					}
+				}
+			} else {
+				if isN && isAdj {
+					ans += n
+				}
+				n = 0
+				isN = false
+				isAdj = false
+			}
+		}
+	}
+
+	return ans
+}
+
+func SolvePart2(lines []string) any {
+	field := fld.NewByteField(lines).AddBorder('.')
+	allGears := map[fld.Pos][]int{}
+	for i, row := range field {
+		isN := false
+		gears := containers.NewSet[fld.Pos]()
+		var n int
+		for j, ch := range row {
+			if aoc.IsDigit(ch) {
+				isN = true
+				n = 10*n + int(ch-'0')
+				for ni := i - 1; ni <= i+1; ni++ {
+					for nj := j - 1; nj <= j+1; nj++ {
+						pos := fld.NewPos(ni, nj)
+						ch := field.Get(pos)
 						if ch == '*' {
-							gears.Add(Point{ni, nj})
+							gears.Add(pos)
 						}
 					}
 				}
@@ -39,34 +65,21 @@ func Solve(lines []string) (ans1, ans2 int) {
 						allGears[g] = append(allGears[g], n)
 					}
 				}
-				if isN && isAdj {
-					ans1 += n
-				}
 				n = 0
 				isN = false
-				isAdj = false
 				clear(gears)
 			}
 		}
 	}
 
+	var ans int
 	for _, nn := range allGears {
 		if len(nn) == 2 {
-			ans2 += nn[0] * nn[1]
+			ans += nn[0] * nn[1]
 		}
 	}
 
-	return ans1, ans2
-}
-
-func SolvePart1(lines []string) any {
-	ans1, _ := Solve(lines)
-	return ans1
-}
-
-func SolvePart2(lines []string) any {
-	_, ans2 := Solve(lines)
-	return ans2
+	return ans
 }
 
 var (
