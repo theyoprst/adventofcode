c98aff1c633f560cbf92122973a5af0d9b86ad2b
Slava Ezhkin <yozhkin@gmail.com>
2024-12-01 07:49:51 +0100

    Add solvers for alternative Qwen32B solutions to Advent of Code 2024 day 1.
    --
    Commit message was generated by necommit.

--- 2024/01/main.go
+++ 2024/01/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/theyoprst/adventofcode/aoc"
 )
@@ -37,9 +39,53 @@ func SolvePart2(lines []string) any {
 	return score
 }
 
+func SolvePart1Qwen32B(lines []string) any {
+	left := make([]int, 0)
+	right := make([]int, 0)
+	for _, line := range lines {
+		parts := strings.Fields(line)
+		left = append(left, atoi(parts[0]))
+		right = append(right, atoi(parts[1]))
+	}
+	sort.Ints(left)
+	sort.Ints(right)
+	totalDistance := 0
+	for i := range left {
+		totalDistance += abs(left[i] - right[i])
+	}
+	return totalDistance
+}
+
+func SolvePart2Qwen32B(lines []string) any {
+	left := make([]int, 0)
+	rightCount := make(map[int]int)
+	for _, line := range lines {
+		parts := strings.Fields(line)
+		left = append(left, atoi(parts[0]))
+		rightCount[atoi(parts[1])]++
+	}
+	similarityScore := 0
+	for _, num := range left {
+		similarityScore += num * rightCount[num]
+	}
+	return similarityScore
+}
+
+func atoi(s string) int {
+	n, _ := strconv.Atoi(s)
+	return n
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
 var (
-	solvers1 = []aoc.Solver{SolvePart1}
-	solvers2 = []aoc.Solver{SolvePart2}
+	solvers1 = []aoc.Solver{SolvePart1, SolvePart1Qwen32B}
+	solvers2 = []aoc.Solver{SolvePart2, SolvePart2Qwen32B}
 )
 
 func main() {
