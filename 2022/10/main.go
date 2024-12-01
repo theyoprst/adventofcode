bbec1390d3071dec1264f781aa86b9ce7cb1134b
Slava Ezhkin <yozhkin@gmail.com>
2024-12-01 10:19:00 +0100

    Implement Advent of Code 2022 day 10 solution.

---
+++ 2022/10/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/theyoprst/adventofcode/aoc"
+	"github.com/theyoprst/adventofcode/must"
+)
+
+func SolvePart1(lines []string) any {
+	registerX := 1
+	cycle := 0
+	signalStrengthSum := 0
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		cycle++
+		if cycle%40 == 20 {
+			signalStrengthSum += cycle * registerX
+		}
+		if fields[0] == "addx" {
+			cycle++
+			if cycle%40 == 20 {
+				signalStrengthSum += cycle * registerX
+			}
+			registerX += must.Atoi(fields[1])
+		}
+	}
+	return signalStrengthSum
+}
+
+func SolvePart2(lines []string) any {
+	registerX := 1
+	cycle := 0
+	crt := make([][]byte, 6)
+	for i := range crt {
+		crt[i] = make([]byte, 40)
+	}
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		drawPixel(crt, cycle, registerX)
+		cycle++
+		if fields[0] == "addx" {
+			drawPixel(crt, cycle, registerX)
+			cycle++
+			registerX += must.Atoi(fields[1])
+		}
+	}
+	var output strings.Builder
+	for _, row := range crt {
+		output.Write(row)
+		output.WriteByte('\n')
+	}
+	return output.String()
+}
+
+func drawPixel(crt [][]byte, cycle, x int) {
+	row := cycle / 40
+	col := cycle % 40
+	if aoc.Abs(col-x) <= 1 {
+		crt[row][col] = '#'
+	} else {
+		crt[row][col] = '.'
+	}
+}
+
+var (
+	solvers1 = []aoc.Solver{SolvePart1}
+	solvers2 = []aoc.Solver{SolvePart2}
+)
+
+func main() {
+	aoc.Main(solvers1, solvers2)
+}
