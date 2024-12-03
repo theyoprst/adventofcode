a5d30e46c5f6e54eb20f07f98327fc8b9704560e
Slava Ezhkin <yozhkin@gmail.com>
2024-12-03 10:13:14 +0100

    Improve readability of regex in countMulSum function using rex package.
    --
    Commit message was generated by necommit.

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"regexp"
 	"strings"
 
+	"github.com/hedhyw/rex/pkg/rex"
 	"github.com/theyoprst/adventofcode/aoc"
 	"github.com/theyoprst/adventofcode/must"
 )
@@ -40,7 +40,28 @@ func removeDisabled(text string) string {
 }
 
 func countMulSum(text string) int {
-	re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+	// Traditional way to declare the regex, not quite readable:
+	// re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+
+	// Functional way to declare the regex: too verbose, but more readable:
+	// re := rex.New(
+	// 	rex.Common.Raw(`mul`),
+	// 	rex.Chars.Single('('),
+	// 	rex.Group.Define(rex.Chars.Digits().Repeat().OneOrMore()),
+	// 	rex.Chars.Single(','),
+	// 	rex.Group.Define(rex.Chars.Digits().Repeat().OneOrMore()),
+	// 	rex.Chars.Single(')'),
+	// ).MustCompile()
+
+	// Best of two worlds: same regex with extra spaces and comments.
+	re := rex.New(rex.Common.RawVerbose(`
+		mul\(      # Beware: spaces are trimmed in RawVerbose, but spaces in the middle are retained
+		           # (a space between "mul" and "\(" will break the regex).
+			(\d+), # Group 1: first number.
+			(\d+)  # Group 2: second number.
+		\)
+	`)).MustCompile()
+
 	sum := 0
 	for _, match := range re.FindAllStringSubmatch(text, -1) {
 		sum += must.Atoi(match[1]) * must.Atoi(match[2])
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.21.3
 require golang.org/x/exp v0.0.0-20231206192017-f3f8817b8deb
 
 require github.com/google/go-cmp v0.6.0
+
+require github.com/hedhyw/rex v1.0.0
