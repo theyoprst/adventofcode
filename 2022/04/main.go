4ea2f062b62f745a0ee635c40f8ac207136120e7
Slava Ezhkin <yozhkin@gmail.com>
2024-12-07 16:19:27 +0100

    2022 day 04 tests.

--- 2022/04/main.go
+++ 2022/04/main.go
@@ -1,20 +1,13 @@
-// 16:43 - 16:53 - 17:00
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-
+	"github.com/theyoprst/adventofcode/aoc"
 	"github.com/theyoprst/adventofcode/must"
 )
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Split(bufio.ScanLines)
-	var ans1, ans2 int
-	for scanner.Scan() {
-		line := scanner.Text()
+func SolvePart1(lines []string) any {
+	var ans int
+	for _, line := range lines {
 		a, b := must.Split2(line, ",")
 		a1Str, a2Str := must.Split2(a, "-")
 		b1Str, b2Str := must.Split2(b, "-")
@@ -28,14 +21,39 @@ func main() {
 		i1 := max(a1, b1)
 		i2 := min(a2, b2)
 		if i1 == a1 && i2 == a2 || i1 == b1 && i2 == b2 {
-			ans1++
-		}
-		if i1 <= i2 {
-			ans2++
+			ans++
 		}
 	}
-	must.NoError(scanner.Err())
+	return ans
+}
 
-	fmt.Println("Part 1:", ans1)
-	fmt.Println("Part 2:", ans2)
+func SolvePart2(lines []string) any {
+	var ans int
+	for _, line := range lines {
+		a, b := must.Split2(line, ",")
+		a1Str, a2Str := must.Split2(a, "-")
+		b1Str, b2Str := must.Split2(b, "-")
+		a1 := must.Atoi(a1Str)
+		a2 := must.Atoi(a2Str)
+		b1 := must.Atoi(b1Str)
+		b2 := must.Atoi(b2Str)
+		must.LessOrEqual(a1, a2)
+		must.LessOrEqual(b1, b2)
+		// Intersect:
+		i1 := max(a1, b1)
+		i2 := min(a2, b2)
+		if i1 <= i2 {
+			ans++
+		}
+	}
+	return ans
+}
+
+var (
+	solvers1 = []aoc.Solver{SolvePart1}
+	solvers2 = []aoc.Solver{SolvePart2}
+)
+
+func main() {
+	aoc.Main(solvers1, solvers2)
 }
