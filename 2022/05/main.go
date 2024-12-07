4e992e531b9518b5f2d85c5f215d50be5090cea5
Slava Ezhkin <yozhkin@gmail.com>
2024-12-07 16:41:01 +0100

    2022 day 05 tests.

--- 2022/05/main.go
+++ 2022/05/main.go
@@ -2,55 +2,77 @@
 package main
 
 import (
-	"fmt"
-	"slices"
-
 	"github.com/theyoprst/adventofcode/aoc"
 	"github.com/theyoprst/adventofcode/must"
 )
 
-func Pop[T any](a []T) ([]T, T) {
-	must.Greater(len(a), 0)
-	return a[:len(a)-1], a[len(a)-1]
+func SolvePart1(lines []string) any {
+	blocks := aoc.Split(lines, "")
+	must.Equal(len(blocks), 2)
+	stacks := parseStacks(blocks[0])
+	steps := blocks[1]
+	for _, step := range steps {
+		nums := aoc.Ints(step)
+		count, src, dst := nums[0], nums[1], nums[2]
+		for j := 0; j < count; j++ {
+			var x byte
+			stacks[src], x = pop(stacks[src])
+			stacks[dst] = append(stacks[dst], x)
+		}
+	}
+
+	var ans []byte
+	for i := 1; i < len(stacks); i++ {
+		ans = append(ans, stacks[i][len(stacks[i])-1])
+	}
+
+	return string(ans)
 }
 
-func main() {
-	lines := aoc.ReadInputLines()
-	emptyIdx := slices.Index(lines, "")
-	n := len(aoc.Ints(lines[emptyIdx-1]))
+func SolvePart2(lines []string) any {
+	blocks := aoc.Split(lines, "")
+	must.Equal(len(blocks), 2)
+	stacks := parseStacks(blocks[0])
+	steps := blocks[1]
+	for _, step := range steps {
+		nums := aoc.Ints(step)
+		count, src, dst := nums[0], nums[1], nums[2]
+		stacks[dst] = append(stacks[dst], stacks[src][len(stacks[src])-count:]...)
+		stacks[src] = stacks[src][:len(stacks[src])-count]
+	}
+
+	var ans []byte
+	for i := 1; i < len(stacks); i++ {
+		ans = append(ans, stacks[i][len(stacks[i])-1])
+	}
+	return string(ans)
+}
+
+func parseStacks(lines []string) [][]byte {
+	n := len(aoc.Ints(lines[len(lines)-1]))
 	stacks := make([][]byte, n+1)
-	stacks2 := make([][]byte, n+1)
-	for i := emptyIdx - 2; i >= 0; i-- {
+	for i := len(lines) - 2; i >= 0; i-- {
 		line := lines[i]
 		for j := 1; j <= n; j++ {
 			letter := line[1+(j-1)*4]
 			if letter != ' ' {
 				stacks[j] = append(stacks[j], letter)
-				stacks2[j] = append(stacks2[j], letter)
 			}
 		}
 	}
-	for i := emptyIdx + 1; i < len(lines); i++ {
-		nums := aoc.Ints(lines[i])
-		count, src, dst := nums[0], nums[1], nums[2]
-		for j := 0; j < count; j++ {
-			var x byte
-			stacks[src], x = Pop(stacks[src])
-			stacks[dst] = append(stacks[dst], x)
-		}
-		stacks2[dst] = append(stacks2[dst], stacks2[src][len(stacks2[src])-count:]...)
-		stacks2[src] = stacks2[src][:len(stacks2[src])-count]
-	}
+	return stacks
+}
 
-	var ans []byte
-	for i := 1; i <= n; i++ {
-		ans = append(ans, stacks[i][len(stacks[i])-1])
-	}
-	fmt.Println("Part 1:", string(ans))
+func pop[T any](a []T) ([]T, T) {
+	must.Greater(len(a), 0)
+	return a[:len(a)-1], a[len(a)-1]
+}
 
-	var ans2 []byte
-	for i := 1; i <= n; i++ {
-		ans2 = append(ans2, stacks2[i][len(stacks2[i])-1])
-	}
-	fmt.Println("Part 2:", string(ans2))
+var (
+	solvers1 = []aoc.Solver{SolvePart1}
+	solvers2 = []aoc.Solver{SolvePart2}
+)
+
+func main() {
+	aoc.Main(solvers1, solvers2)
 }
