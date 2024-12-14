d3f272333493290859deec15cd36b6afe7fb8595
Slava Ezhkin <yozhkin@gmail.com>
2024-12-14 17:33:54 +0100

    2024 day 14: fast solution.

--- 2024/14/main.go
+++ 2024/14/main.go
@@ -97,6 +97,68 @@ func SolvePart2(lines []string) any {
 	}
 }
 
+func SolvePart2Fast(lines []string) any {
+	const width = 101
+	const height = 103
+
+	var x []int
+	var y []int
+	var dx []int
+	var dy []int
+	for _, line := range lines {
+		a := aoc.Ints(line)
+		x = append(x, a[0])
+		y = append(y, a[1])
+		dx = append(dx, a[2])
+		dy = append(dy, a[3])
+	}
+
+	// Use the fact that situations in the grid have cycle 101*103: because 101 and 103 are primes,
+	// and each robot cycles it's x coordinate in 101 steps, and y in 103 steps).
+	// So we can iterate over 101*103 steps and find the minimum std dev / entropy / whatever relevane metric.
+	// But we can only do 103 iteractions to find the best entropies for both x and y.
+	// And then find the right time knowing the modulos by the width and the height.
+	// Kudos: https://www.reddit.com/r/adventofcode/comments/1hdvhvu/comment/m1zws1g/
+
+	var bestTimeX, bestTimeY int
+	bestVarX := math.MaxInt64
+	for i := range width {
+		for j := range x {
+			x[j] = (x[j] + dx[j] + width) % width
+		}
+		varX := varianceN(x)
+		if varX < bestVarX {
+			bestVarX = varX
+			bestTimeX = i
+		}
+	}
+
+	bestVarY := math.MaxInt64
+	for i := range height {
+		for j := range y {
+			y[j] = (y[j] + dy[j] + height) % height
+		}
+		varY := varianceN(y)
+		if varY < bestVarY {
+			bestVarY = varY
+			bestTimeY = i
+		}
+	}
+
+	// t = bestVarX (mod width)
+	// t = bestVarY (mod height)
+
+	// Original solution: use inverse by mod (shoud be implemented):
+	// return 1 + bestTimeX + ((inverse(width, height)*(bestTimeY-bestTimeX+height))%height)*width
+
+	for t := range width * height {
+		if t%width == bestTimeX && t%height == bestTimeY {
+			return t + 1
+		}
+	}
+	panic("no time found")
+}
+
 func parsePV(s string) (fld.Pos, fld.Pos) {
 	a := aoc.Ints(s)
 	must.Equal(len(a), 4)
@@ -117,9 +179,24 @@ func stddev(a []int) float64 {
 	return math.Sqrt(variance / float64(len(a)))
 }
 
+// returns variance(a) * len(a)
+func varianceN(a []int) int {
+	mean := 0
+	for _, v := range a {
+		mean += v
+	}
+	mean /= len(a)
+	variance := 0
+	for _, v := range a {
+		diff := v - mean
+		variance += diff * diff
+	}
+	return variance
+}
+
 var (
 	solvers1 = []aoc.Solver{SolvePart1}
-	solvers2 = []aoc.Solver{SolvePart2}
+	solvers2 = []aoc.Solver{SolvePart2, SolvePart2Fast}
 )
 
 func main() {
