f910bcdacdcda7c2acb2595c4f5e4e6dcf67a449
Slava Ezhkin <yozhkin@gmail.com>
2024-12-15 08:36:18 +0100

    Imlement solution for 2024 day 15.

---
+++ 2024/15/main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/theyoprst/adventofcode/aoc"
+	"github.com/theyoprst/adventofcode/aoc/containers"
+	"github.com/theyoprst/adventofcode/aoc/fld"
+)
+
+func SolvePart1(lines []string) any {
+	blocks := aoc.Blocks(lines)
+	field := fld.NewByteField(blocks[0])
+	robotPos := field.FindFirst('@')
+	for _, dir := range parseCommands(blocks[1]) {
+		// find first no box space in the direction dir:
+		noBoxPos := robotPos.Add(dir)
+		for field.Get(noBoxPos) == 'O' {
+			noBoxPos = noBoxPos.Add(dir)
+		}
+		if field.Get(noBoxPos) == '.' {
+			field.Set(noBoxPos, 'O')
+			field.Set(robotPos, '.')
+			field.Set(robotPos.Add(dir), '@')
+			robotPos = robotPos.Add(dir)
+		}
+	}
+	return score(field, 'O')
+}
+
+func SolvePart2(lines []string) any {
+	blocks := aoc.Blocks(lines)
+	field := fld.NewByteField(enlarge(blocks[0]))
+	robotPos := field.FindFirst('@')
+
+	// toPush returns list of items to push is topological reverse sorted order (the further item is the first in the list).
+	// toPush returns empty list if there is a wall in the way.
+	toPush := func(dir fld.Pos) []fld.Pos {
+		visited := containers.NewSet[fld.Pos]()
+		var topRevSorted []fld.Pos
+		canPush := true
+
+		var dfs func(from fld.Pos)
+		dfs = func(from fld.Pos) {
+			if visited.Has(from) {
+				return
+			}
+			visited.Add(from)
+			next := from.Add(dir)
+			switch field.Get(next) {
+			case '[':
+				dfs(next)
+				dfs(next.Add(fld.Right))
+			case ']':
+				dfs(next)
+				dfs(next.Add(fld.Left))
+			case '#':
+				canPush = false
+			}
+			topRevSorted = append(topRevSorted, from)
+		}
+
+		dfs(robotPos)
+		if !canPush {
+			return nil
+		}
+		return topRevSorted
+	}
+
+	for _, dir := range parseCommands(blocks[1]) {
+		for _, src := range toPush(dir) {
+			// Because items to push are sorted (the further item is the first in the list),
+			// there is no overlapping during pushing.
+			field.Swap(src, src.Add(dir))
+			if src == robotPos {
+				robotPos = robotPos.Add(dir)
+			}
+		}
+	}
+
+	return score(field, '[')
+}
+
+func parseCommands(lines []string) []fld.Pos {
+	var commands []fld.Pos
+	for _, command := range strings.Join(lines, "") {
+		var dir fld.Pos
+		switch command {
+		case '^':
+			dir = fld.Up
+		case 'v':
+			dir = fld.Down
+		case '<':
+			dir = fld.Left
+		case '>':
+			dir = fld.Right
+		default:
+			panic("invalid command")
+		}
+		commands = append(commands, dir)
+	}
+	return commands
+}
+
+func enlarge(lines []string) []string {
+	var newLines []string
+	for i := range lines {
+		var sb strings.Builder
+		for j := range len(lines[i]) {
+			switch lines[i][j] {
+			case '#':
+				sb.WriteString("##")
+			case 'O':
+				sb.WriteString("[]")
+			case '.':
+				sb.WriteString("..")
+			case '@':
+				sb.WriteString("@.")
+			}
+		}
+		newLines = append(newLines, sb.String())
+	}
+	return newLines
+}
+
+func score(field fld.ByteField, boxCh byte) int {
+	sum := 0
+	for row := range field.Rows() {
+		for col := range field.Cols() {
+			pos := fld.NewPos(row, col)
+			if field.Get(pos) == boxCh {
+				sum += 100*row + col
+			}
+		}
+	}
+	return sum
+}
+
+var (
+	solvers1 = []aoc.Solver{SolvePart1}
+	solvers2 = []aoc.Solver{SolvePart2}
+)
+
+func main() {
+	aoc.Main(solvers1, solvers2)
+}
