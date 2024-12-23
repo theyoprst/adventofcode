b45b6057590640c8519b58de53c83e62232d770e
Slava Ezhkin <yozhkin@gmail.com>
2024-12-23 20:25:11 +0100

    Add more set methods.

--- aoc/containers/set.go
+++ aoc/containers/set.go
@@ -60,3 +60,18 @@ func (s Set[K]) Difference(other Set[K]) Set[K] {
 	}
 	return difference
 }
+
+// Update adds all keys from the other set to the current set.
+func (s Set[K]) Update(other Set[K]) {
+	for key := range other {
+		s[key] = struct{}{}
+	}
+}
+
+// Union returns a new set with keys that are present in either set.
+func (s Set[K]) Union(other Set[K]) Set[K] {
+	union := NewSet[K]()
+	union.Update(s)
+	union.Update(other)
+	return union
+}
